1077443bf9e5c7a05a4cda37f119bdeba0453108
rabiul-idb <rabiul.idb40@gmail.com>
2025-04-12 09:41:58 +0600

    counter alignment updated

--- js/main.js
+++ js/main.js
@@ -302,7 +302,27 @@
         infinite: true,
         customPaging: function(slider, i) {
           return '<button class="custom-dot"></button>';
-        }
+        },
+        responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+             
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       });
 
 
